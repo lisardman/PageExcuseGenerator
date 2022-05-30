047370ef966de753731110c66311e8da836178c7
lisardman <lisardman@gmail.com>
2022-05-30 11:08:57 +0200

    Add files via upload

---
+++ excuse.py
@@ -0,0 +1,15 @@
+import random
+
+page_excuse = {
+        "Äta med morsan",
+        "älta med surban",
+        "hongirerhonhirerhongrier",
+        "redan ätit",
+        "löda dö",
+        "götet",
+        "har käkat brunch nyss"
+}
+
+key = random.choice(list(page_excuse))
+
+print(key)
