8f4b23261263da5046cea0aa6d015056a1fa34a1
Mikael Karlsson <lisardman@gmail.com>
2023-02-06 22:00:42 +0100

    added start.sh for simple start of flask application.
    added template for making homepage "more" fine
    
    Committer: Mikael Karlsson <lisardman@gmail.com>

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 Generates Excuses for skipping lunch
 
 
-to run webapp. 
+to run webapp or run start.sh  ./start.sh in the "web"-folder
 FLASK_APP=peg.py flask run
 
 then you can access it browsing http://127.0.0.1:5000
 
- 
+
--- web/peg.py
+++ web/peg.py
@@ -1,17 +1,24 @@
-from flask import Flask
+from flask import Flask,render_template
 import random
 
 app = Flask(__name__)
 
-rainbow = "\U0001F308"
+#rainbow = "\U0001F308"
 
 @app.route('/')
+def index():
+    excuse = get_excuse()
+    return render_template("template.html",excuse=excuse)
+
 def get_excuse():
     with open("excuses.txt") as f:
         excuses = f.read().splitlines()
     key = random.choice(excuses)
-    return rainbow + "   " + key
+    return "   " + key
+
+def create_rainbow():
+    rainbow = "\U0001F308"
+    return rainbow
 
 if __name__ == '__main__':
     app.run()
-
